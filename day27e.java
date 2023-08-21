89a9c0f13f4f23cb265a2bf772b34ea43af619ad
Rasmilakc <139522774+Rasmilakc@users.noreply.github.com>
2023-08-20 22:00:16 -0500

    Add files via upload

---
+++ day27e.java
@@ -0,0 +1,53 @@
+package test4;
+
+public class Day27e {
+
+	public static void main(String[] args) {
+		//5.Define example of overrriding
+		SB A=new SB("america", "waco");
+		A.loan();
+		A.save();
+		A.displaymessage();
+} 
+}
+class WB{
+	String Texas;
+	public WB(String tx) {
+		this.Texas=tx;
+	}public void loan() {
+		System.out.println("loan method");
+	}public void save() {
+		System.out.println("save method");
+	}public void displaymessage() {
+		System.out.println("I am from america");
+	}
+}
+class NB1 extends WB{
+	String chabhil;
+
+	public NB1(String tx,String ch) {
+		super(tx);
+		this.chabhil=ch;
+	}public void loan() {
+		System.out.println("loan method");
+	}public void save() {
+		System.out.println("save method");
+	}
+}
+class SB extends WB{
+	String branch;
+
+	public SB(String tx,String bn) {
+		super(tx);
+		this.branch=bn;
+	}
+	public void loan() {
+		System.out.println("loan method from waco");
+	}public void save() {
+		System.out.println("save method from waco ");
+	}
+
+
+	}
+
+
